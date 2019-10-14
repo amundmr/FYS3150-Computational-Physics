8263b5973ccd60f8085b5a4537a794a4b6fc4b6e
annasro <anna.stray.rongve@gmail.com>
2019-10-14 11:22:48 +0200

    monte carlo

---
+++ Programcode/monte_carlo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+int uniform_random_generator(double x, int n){
+  int a = 0, b = 1, i;
+  int t [100], p [100];
+
+    if ( x < 0 ){
+        t [double x] = 0.0;
+    }
+    else (t [x] = 1 / (b-a))
+
+    p[x] = (theta*theta*(x-a)(b-x))/(a-b);
+
+
+  }
+
+
+
+int main(){
+
+  double x,y,z;
+  int a, b, c, i, j;
+
+}
