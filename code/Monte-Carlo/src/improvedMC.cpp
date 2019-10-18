9f5b2024d05ee70e4cf819c07f505796af22cdeb
amundmr <amundmr@github.com>
2019-10-18 15:19:57 +0200

    The paralellization is perfect now

--- code/Monte-Carlo/src/improvedMC.cpp
+++ code/Monte-Carlo/src/improvedMC.cpp
@@ -2,17 +2,22 @@
 
 // Monte Carlo integration with spherical coordinates, where r is distributed along the exponential distribution.
 double improvedMC(double infty,int MCsamples){
-    double mc, var, fval, sigma, int_mc; vec r1 = zeros<vec>(3); vec r2 = zeros<vec>(3); // Variable storage.
+    double mc, var, fval, sigma, int_mc; // Variable storage.
 
     double V = pow(M_PI,4)/4; // Weighted integration volume.
 
-    // Initialize RNG (Mersenne Twister) in our intervals.
-    mt19937::result_type seed = time(0);
-    auto r_rand = bind(uniform_real_distribution<double>(0,1),mt19937(seed));
-    auto costheta_rand = bind(uniform_real_distribution<double>(-1,1),mt19937(seed));
-    auto phi_rand = bind(uniform_real_distribution<double>(0,2*M_PI),mt19937(seed));
+    #pragma omp parallel
+    {
 
+    // Initialize RNG (Mersenne Twister) in our intervals.
+    mt19937::result_type seedu = time(0) + omp_get_thread_num();
+    auto r_rand = bind(uniform_real_distribution<double>(0,1),mt19937(seedu));
+    auto costheta_rand = bind(uniform_real_distribution<double>(-1,1),mt19937(seedu));
+    auto phi_rand = bind(uniform_real_distribution<double>(0,2*M_PI),mt19937(seedu));
+
+    #pragma omp for reduction(+:mc) reduction(+:var)
     for (int i=0 ; i<MCsamples ; i++){
+        vec r1 = zeros<vec>(3); vec r2 = zeros<vec>(3);
         // Fill vectors with random values in interval.
         r1(0) = -1.0/4 * log(1-r_rand()); r1(1) = costheta_rand(); r1(2) = phi_rand();
         r2(0) = -1.0/4 * log(1-r_rand()); r2(1) = costheta_rand(); r2(2) = phi_rand();
@@ -20,7 +25,8 @@ double improvedMC(double infty,int MCsamples){
         // Add function value to sum.
         mc += f2(r1,r2);
         var += f2(r1,r2) * f2(r1,r2);
-    }
+    }//
+  }
     // Find mean.
     mc /= MCsamples;
     // Find variance
