13ce026df1d8a629ceaa8e8714f3f3adf5804c32
annasro <anna.stray.rongve@gmail.com>
2019-10-25 19:48:35 +0200

    flere program til oppgave b

--- code/Ising/main.cpp
+++ code/Ising/main.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
-#include <cmath>
-#include <ctime>
-#include <random>
-#include <cstdlib>
-#include <stdio.h>
 #include <fstream>
 #include <iomanip>
+#include <vector>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+//#include "lib.h"
 using namespace std;
 ofstream ofile;
 
 inline int periodic_boundary_conditions(int &i, int &limit, int &add){
   return (i+limit+add) % (limit);
 }
-
-void initilize(int, double, int **, double&, double&);
+inline int periodic_boundary_conditions(int &, int &, int&);
+//read in input in terminal
 void input(int &, int & , double &, double &, double &);
+//initilize energy and magnetization
+void initilize(int, double, int **, double&, double&);
+//Metropolis algo
+void Metropolis(int , long&, int **, double &, double &, double *);
+//Results are by this function printed to a file;
+void output(int, int, double, vector);
 
+//Main program
 int main(int argc, char* argv[] ){
 
-  int **spinn, L, mcs;
+  int **spinn, L, mcs,w[17], avarage[5];
   double T, T_start, T_end, T_step, E, M;
   char *outfilename;
   long idum;
 
+  //Input arguments from command line. Aborts if there are too few
   if (argc <= 1){
     cout<<"Wrong" << argv[0];
     cout <<"Write also output filename on same line" << endl;
+    exit(1);
   }
   else {
     outfilename = argv[1];
   }
 
-  ofile.open(outfilename)
+  ofile.open(outfilename);
   input(L, mcs, T_start, T_end, T_step);
   spinn = (int**) matrix(L,L, sizeof(int));
   idum = -1; //random starting point
@@ -39,9 +48,28 @@ int main(int argc, char* argv[] ){
   for (T = T_start; T<= T_end; T += T_step){
     E = M = 0.;
 
+    for (int de = -8; de <= 8 ;de++) w[de+8] = 0;
+    for (int de = -8; de <= 8 ;de+=4) w[de+8] = exp(-de/T);
 
+    //Array for expectation values
+    for(int i = 0; i < 5; i++) avarage[i] = 0;
+    initilize(L, double T, spinn, E, M);
+
+    //Monte Carlo:
+      for (int cycles = 1; cycles <= mcs; cycles++){
+        Metropolis(L,idum,spinn,E,M,w);
+
+        avarage[0] += E; avarage[1] += E*E;
+        avarage[2] += M; avarage[3] += M*M; avarage[4] += fabs(M);
+
+
+      }
+
+      //print results;
+      output(L,mcs,T,avarage);
   }
 
-
-
+  free_matrix((void**) spinn);
+  ofile.close();
+  return 0;
 }
