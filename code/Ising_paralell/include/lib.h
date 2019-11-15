eacf61bae181203a25927f5aab19843801f52fc2
amundmr <amundmr@github.com>
2019-11-15 16:08:11 +0100

    Paralelliserte anna sin kode inni _paralell

---
+++ code/Ising_paralell/include/lib.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <random>
+#include <iomanip>
+#include <cstdlib>
+#include <armadillo>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+using namespace std;
+using namespace arma;
+
+void initialize(int, double, mat &, double&, double&);
+void Metropolis(int, long&, mat &, double&, double&, vec,ofstream & file2, int &cycles, int &sum, int mcs);
+void input(int&, int& , double&, double&, double&);
+void output(int, int, double,  ofstream & file, double, double, double, double, double);
+int periodic_boundary_conditions(int, int, int);
+void initialize_random(int L, double T, mat & spin, double & E, double & M);
---
+++ code/Ising_paralell/src/output.cpp
@@ -0,0 +1,30 @@
+#include "../include/lib.h"
+
+void output(int L, int mcs, double T, ofstream & file, double E_avg, double M_avg, double EE_avg, double MM_avg, double Mfabs){
+
+  double norm = 1.0/((double) (mcs));
+  double s_norm = 1.0/(L*L);
+  double E_ExpectVal    = E_avg*norm;
+  double E2_ExpectVal   = EE_avg*norm;
+  double M_ExpectVal    = M_avg*norm;
+  double M2_ExpectVal   = MM_avg*norm;
+  double Mabs_ExpectVal = Mfabs*norm;
+
+  double Evariance =(E2_ExpectVal-E_ExpectVal*E_ExpectVal)/L/L;
+  double Mvariance =(M2_ExpectVal-Mabs_ExpectVal*Mabs_ExpectVal)/L/L;
+  double Cv = (E2_ExpectVal-E_ExpectVal*E_ExpectVal)/T/T*s_norm;
+  double susep = (M2_ExpectVal-M_ExpectVal*M_ExpectVal)/T*s_norm;
+
+  //file << setiosflags(ios::showpoint | ios::uppercase);
+  //file <<"           Temperature: "  << setw(15) << setprecision(8) << T <<endl;
+  //file <<"       Energy variance: "  << setw(15) << setprecision(8) << Evariance/T/T<<endl;
+  //file <<"Expected magnetisation: "  << setw(15) << setprecision(8) << M_ExpectVal/L/L<<endl;
+  //file <<"     Magnetic variance: "  << setw(15) << setprecision(8) << Mvariance/T<<endl;
+  //file <<"       Expected energy: "  << setw(15) << setprecision(8) << E_ExpectVal/L/L<<endl;
+  //file << "Absolute magnetisation: "  << setw(15) << setprecision(8) << Mabs_ExpectVal/L/L<<endl;
+  //file <<"     Heat capacity, Cv: "  << setw(15) << setprecision(8) << Cv<< endl;
+  //file <<"   Susceptibility, chi: "  << setw(15) << setprecision(8) << susep;
+
+  file << mcs << " " << Mabs_ExpectVal/L/L << " " << E_ExpectVal/L/L << endl;
+
+}
