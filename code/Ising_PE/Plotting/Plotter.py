ee7449f0fe99908f501fcea80a8e5466091fdeb9
amundmr <amund.raniseth@gmail.com>
2019-11-17 19:18:04 +0100

    Made result of variance in probdens work really good

--- code/Ising_PE/Plotting/Plotter.py
+++ code/Ising_PE/Plotting/Plotter.py
@@ -33,6 +33,13 @@ plt.hist(Alldata[10000:],bins = 50, density=True) #bins=30
 plt.ylabel('Probability density')
 plt.xlabel('Energy')
 plt.title(r'Energy Probability distribution for $T=1$ with %g MC cycles' %N)
+
+std1 = np.std(Alldata[10000:])
+mean1 = np.mean(Alldata[10000:])
+plt.axvline(x=mean1-std1, linestyle = "--", color ='r', label = r"Standard deviation: %.2f"%std1)
+plt.axvline(x=mean1, linestyle = "--", color ='g', label = r"Mean: %.2f"%mean1)
+plt.axvline(x=mean1+std1, linestyle = "--", color ='r')
+plt.legend()
 plt.savefig("../ProbE_T1.png", bbox_inches='tight')
 plt.show()
 
@@ -40,8 +47,13 @@ plt.hist(Alldata2[10000:],bins = 120, density=True) #bins=30
 plt.ylabel('Probability density')
 plt.xlabel('Energy')
 plt.title(r'Energy Probability distribution for $T=2.4$ with %g MC cycles' %N2)
-plt.axvline(x=-550, linestyle = "--", color ='r')
-plt.axvline(x=-430, linestyle = "--", color ='r')
+std = np.std(Alldata2[10000:])
+mean = np.mean(Alldata2[10000:])
+plt.axvline(x=mean-std, linestyle = "--", color ='r', label = r"Standard deviation: %.2f"%std)
+plt.axvline(x=mean, linestyle = "--", color ='g', label = r"Mean: %.2f"%mean)
+plt.axvline(x=mean+std, linestyle = "--", color ='r')
+
+plt.legend()
 plt.savefig("../ProbE_T24.png", bbox_inches='tight')
 
 plt.show()
