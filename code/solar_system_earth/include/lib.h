9cc434cec247cd91b380fcac10c6bba3cfba4897
annasro <anna.stray.rongve@gmail.com>
2019-11-29 18:14:44 +0100

    first round of code. Make + headerfile

---
+++ code/solar_system_earth/include/lib.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <random>
+#include <iomanip>
+#include <cstdlib>
+#include <armadillo>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+using namespace arma;
+
+void output(double, double, double, double, double);
---
+++ code/solar_system_earth/src/output.cpp
@@ -0,0 +1,9 @@
+void output(double time, double x, double y, double vx, double vy)
+{
+ofile << setiosflags(ios::showpoint | ios::uppercase);
+ofile << setw(15) << setprecision(8) << time;
+ofile << setw(15) << setprecision(8) << x;
+ofile << setw(15) << setprecision(8) << y;
+ofile << setw(15) << setprecision(8) << vx;
+ofile << setw(15) << setprecision(8) << vy << endl;
+}
