f12def3589e11ff489e121c0f1b47ca3536d22f9
amundmr <amund.raniseth@gmail.com>
2019-11-30 11:03:11 +0100

    Desperate try for Verlet method on task b!

---
+++ code/OO_Earth-Sun/include/lib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <armadillo>
+
+using namespace std;
+
+
+void output(double, double, double, double, double,ofstream& );
