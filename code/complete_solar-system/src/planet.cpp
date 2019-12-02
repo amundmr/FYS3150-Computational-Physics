395aabe4c65003e6b7ad4861ce551ccaea894798
kmaasrud <kmaasrud@outlook.com>
2019-12-02 12:21:32 +0100

    Separated my efforts. On track to general solver.

---
+++ code/complete_solar-system/src/planet.cpp
@@ -0,0 +1,37 @@
+#include "./include/lib.h"
+
+class Planet
+{
+private:
+    arma::vec r;
+    arma::vec v;
+    double m;
+public:
+    // Default constructor
+    Planet()
+    {
+        m = 1; r = arma::zeros(3); v = arma::zeros(3);
+    }
+    // Constructor with init values.
+    Planet(double mass, arma::vec init_r, arma::vec init_v){
+        m = mass; r = init_r; v = init_v;
+    };
+
+    // Distance between two planets.
+    double d(Planet & planet)
+    {
+        double x = this->r(0)-planet.r(0);
+        double y = this->r(1)-planet.r(1);
+        double z = this->r(2)-planet.r(2);
+        return sqrt(x*x+y*y+z*z);
+    };
+    
+    // Acceleration
+    double a(Planet & planet)
+    {
+        double d = d(planet);
+        if (d!=0){
+            return G*planet.m/(d*d);
+        }
+    };
+};
--- code/solar_system_earth/src/euler.cpp
+++ code/solar_system_earth/src/euler.cpp
@@ -2,10 +2,10 @@
 // Function takes in empty position and velocity vectors.
 // In addition to this, it takes a pointer to the force affecting the object,
 // as well as some initial conditions and the time-step characteristics.
-void euler(arma::vec & r, arma::vec & v, double (*a)(double), double init_r, double init_v, double dt, int N)
+void euler(arma::mat & r, arma::mat & v, double (*a)(double), arma::vec init_r, arma::vec init_v, double dt, int N)
 {
-    r = arma::zeros<arma::vec>(N); r(0) = init_r; // Filling the position vector with initial condition and zeros.
-    v = arma::zeros<arma::vec>(N); v(0) = init_v; // Filling the velocity vector with initial condition and zeros.
+    r = arma::zeros<arma::mat>(N,3); r(0,0) = init_r(0); r(0,1) = init_r(1); r(0,2) = init_r(2); // Filling the position vector with initial condition and zeros.
+    v = arma::zeros<arma::mat>(N,3); v(0,0) = init_v(0); v(0,1) = init_v(1); v(0,2) = init_v(2); // Filling the velocity vector with initial condition and zeros.
 
     // Euler method
     for(int i=0; i<N-1; i++)
