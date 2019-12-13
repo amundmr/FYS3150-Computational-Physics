010229d067646a69d9881e91e7e5145517399e6d
amundmr <amundmr@github.com>
2019-12-13 11:35:38 +0100

    Fixed jupiter plotting

--- code/Plotter/2d-plot_jupiter.py
+++ code/Plotter/2d-plot_jupiter.py
@@ -5,26 +5,26 @@ import sys
 
 
 earth_x = []; earth_y = []; earth_z = []
-with open('../complete_solar-system/output/jupiter/Earthg.txt', 'r') as f:
+with open('../complete_solar-system/output/jupiter/Earthg1000.txt', 'r') as f:
     for line in f:
         l = line.split("\t")
         earth_x.append(float(l[0])); earth_y.append(float(l[1])); earth_z.append(float(l[2]))
 
 jupiter_x = []; jupiter_y = []; jupiter_z = []
-with open('../complete_solar-system/output/jupiter/Jupiterg.txt', 'r') as f:
+with open('../complete_solar-system/output/jupiter/Jupiterg1000.txt', 'r') as f:
     for line in f:
         l = line.split("\t")
         jupiter_x.append(float(l[0])); jupiter_y.append(float(l[1])); jupiter_z.append(float(l[2]))
 ##
 
 sun_x = []; sun_y = []; sun_z = []
-with open('../complete_solar-system/output/jupiter/Sung.txt', 'r') as f:
+with open('../complete_solar-system/output/jupiter/Sung1000.txt', 'r') as f:
     for line in f:
         l = line.split("\t")
         sun_x.append(float(l[0])); sun_y.append(float(l[1])); sun_z.append(float(l[2]))
 
 Plotskip = 10 #Plot only N'th point in the vectors
-dotsize = 3
+dotsize = 2
 
 #plt.scatter(sun_x[::Plotskip], sun_y[::Plotskip], s=dotsize, label = "Sun", color = "orange")
 #plt.scatter(earth_x[::Plotskip], earth_y[::Plotskip],  s=dotsize, label = "Earth", color = "blue")
@@ -36,9 +36,14 @@ dt = yrs/(len(earth_x))
 
 #Plotting read data
 cm = cm.get_cmap('gist_rainbow')
-sc = plt.scatter(earth_x[::Plotskip], earth_y[::Plotskip],s=dotsize, label= "Earth", c = t, cmap = cm)
-sc2 = plt.scatter(jupiter_x[::Plotskip], jupiter_y[::Plotskip],s=dotsize, label= "Jupiter", c = t, cmap = cm)
-sc1 = plt.scatter(sun_x[::Plotskip], sun_y[::Plotskip],s=dotsize, label= "Sun", c = t, cmap = cm)
+plt.plot(sun_x[::Plotskip], sun_y[::Plotskip],linewidth=dotsize*3, label= "Sun", color='r', zorder=0)
+sc1 = plt.scatter(sun_x[::Plotskip], sun_y[::Plotskip],s=dotsize, c = t, cmap = cm, zorder=1)
+
+plt.plot(jupiter_x[::Plotskip], jupiter_y[::Plotskip],linewidth=dotsize*3, label= "Jupiter", color='b', zorder=3)
+sc2 = plt.scatter(jupiter_x[::Plotskip], jupiter_y[::Plotskip],s=dotsize, c = t, cmap = cm, zorder=4)
+
+sc = plt.scatter(earth_x[::Plotskip], earth_y[::Plotskip],s=dotsize, label = "Earth", c = t, cmap = cm, zorder=5)
+
 cb = plt.colorbar(sc)
 cb.set_label("Years after simulation start.")
 
@@ -47,10 +52,10 @@ cb.set_label("Years after simulation start.")
 plt.ylabel("Y Position, [AU]")
 plt.xlabel("X Position, [AU]")
 plt.tick_params(direction='in', top = 'true', right = 'true')
-plt.title(r"Earth and Jupiter, $M_{Jupiter}=9.54E-4$, $\Delta t = %g yr$"%dt)
+plt.title(r"Earth and Jupiter, $M_{Jupiter}=9.54E-1$, $\Delta t = %g yr$"%dt)
 #plt.ylim(0,13.5)
 #plt.yscale('log')
-#plt.legend(loc="upper left")
+plt.legend()
 
 plt.savefig('Earth-Sun_Test.png', bbox_inches='tight')
 plt.show()
