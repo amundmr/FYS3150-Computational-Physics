f97cbace35f7c0ed92cada662bb1005bc7acd3cb
amundmr <amundmr@github.com>
2019-12-17 13:22:32 +0100

    Added inner planets plot, touched up a lot of things.. Realised what i wrote 3 days ago was pretty bad.

--- code/Plotter/3d-plot.py
+++ code/Plotter/3d-plot.py
@@ -33,7 +33,7 @@ colors = {
 
 
 # Storing paths, names and color to all the output files.
-path = "../complete_solar-system/output/allPlanets/"
+path = "../complete_solar-system/output/innerPlanets/"
 outputs = {
     'path': [],
     'name': [],
@@ -56,12 +56,12 @@ for i in range(len(outputs['path'])):
             x.append(float(l[0])); y.append(float(l[1])); z.append(float(l[2]))
 
         ax.scatter(x[::Plotskip], y[::Plotskip], z[::Plotskip], s=dotsize, label = outputs['name'][i], color = outputs['color'][i])
-
+ax.scatter(0,0,s = 60, c = "#f5ed00")
 axisEqual3D(ax)
 ax.set_xlabel('x (AU)')
 ax.set_ylabel('y (AU)')
 ax.set_zlabel('z (AU)')
 ax.view_init(60, 35)
 plt.legend()
-plt.savefig('allPlanets.png', bbox_inches='tight')
+plt.savefig('innerPlanets.png', bbox_inches='tight')
 plt.show()
